8b1a6a699ad049441eede8adc462be8d844ec633
jian zhiqiang <2631095834@qq.com>
2021-05-25 22:36:02 +0800

    项目初始化

---
+++ order/src/main/java/com/jzq/order/OrderApplication.java
@@ -0,0 +1,16 @@
+package com.jzq.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jian zhi qiang
+ * @date 2021/5/25 22:09
+ * @description 
+ */
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
